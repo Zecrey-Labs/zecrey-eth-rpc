72fcdf5bb933383e997d076538f3963f87ed0a06
Gavin <tianyaogavin@gmail.com>
2022-09-01 10:37:53 +0800

    add value param in transact ops contruct method

--- zecrey/core/zecrey-legend/util.go
+++ zecrey/core/zecrey-legend/util.go
@@ -1,6 +1,7 @@
 package zecreyLegend
 
 import (
+	"errors"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/zecrey-labs/zecrey-eth-rpc/_rpc"
 	"math/big"
@@ -20,5 +21,27 @@ func ConstructTransactOpts(cli *_rpc.ProviderClient, authCli *_rpc.AuthClient, g
 	transactOpts.GasLimit = gasLimit
 	transactOpts.From = authCli.Address
 	transactOpts.Value = big.NewInt(0)
+	return transactOpts, nil
+}
+
+
+func ConstructTransactOptsWithValue(cli *_rpc.ProviderClient, authCli *_rpc.AuthClient, gasPrice *big.Int, gasLimit uint64, value int64) (transactOpts *bind.TransactOpts, err error) {
+	transactOpts, err = bind.NewKeyedTransactorWithChainID(authCli.PrivateKey, authCli.ChainId)
+	if err != nil {
+		return nil, err
+	}
+	nonce, err := cli.GetPendingNonce(authCli.Address.Hex())
+	if err != nil {
+		return nil, err
+	}
+	if value < 0 {
+		return nil, errors.New("value is negative")
+	}
+
+	transactOpts.Nonce = big.NewInt(int64(nonce))
+	transactOpts.GasPrice = gasPrice
+	transactOpts.GasLimit = gasLimit
+	transactOpts.From = authCli.Address
+	transactOpts.Value = big.NewInt(value)
 	return transactOpts, nil
 }
